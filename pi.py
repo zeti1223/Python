af9213b4006abbfa777011f1c77d7d87c07d702f
Zeti_1223 <121749710+zeti1223@users.noreply.github.com>
2025-04-21 19:02:43 +0200

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,21 +1,20 @@
 # Initialize denominator
 k = 1
- 
+
 # Initialize sum
 s = 0
 
-range = 1000000
- 
-for i in range(range):
- 
+iterations = 1000000  # Rename variable to avoid conflict with built-in range()
+
+for i in range(iterations):
     # even index elements are positive
     if i % 2 == 0:
-        s + = 4/k
+        s += 4/k  # Fix spacing issue
     else:
         # odd index elements are negative
         s -= 4/k
- 
+
     # denominator is odd
     k += 2
-     
+
 print(s)
